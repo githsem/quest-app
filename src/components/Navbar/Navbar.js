916e34684b5ddb006681fc4fbf57d6b8da2712d0
Hasan Huseyin Semiz <hhsem49@gmail.com>
2022-12-13 01:50:34 +0100

    update

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -12,28 +12,28 @@ import MenuIcon from '@mui/icons-material/Menu';
 function Navbar() {
     let userId = 5;
 
-    
-    return(
+
+    return (
         <div>
             <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 ,textAlign:'left' }}>
-          <Link style={{ textDecoration: 'none', boxShadow:'none', color:'white' }} to="/">Home</Link>
-          </Typography>
-          <Typography variant="h6" component="div" sx={{textAlign:'right' }}>
-          <Link style={{ textDecoration: 'none', boxShadow:'none', color:'white'}}to={{ pathname : '/users/' +userId}}>User</Link>
-          </Typography>
-        </Toolbar>
-      </AppBar>
+                <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1, textAlign: 'left' }}>
+                        <Link style={{ textDecoration: 'none', boxShadow: 'none', color: 'white' }} to="/">Home</Link>
+                    </Typography>
+                    <Typography variant="h6" component="div" sx={{ textAlign: 'right' }}>
+                        <Link style={{ textDecoration: 'none', boxShadow: 'none', color: 'white' }} to={{ pathname: '/users/' + userId }}>User</Link>
+                    </Typography>
+                </Toolbar>
+            </AppBar>
         </div>
     )
 }
