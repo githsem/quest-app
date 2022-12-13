d754b0e706a5b04196135137b31b855f750710a7
Hasan Huseyin Semiz <hhsem49@gmail.com>
2022-12-13 01:50:37 +0100

    update

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -13,78 +13,80 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import CommentIcon from '@mui/icons-material/Comment';
 import { Link } from "react-router-dom";
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 
 
 
 const ExpandMore = styled((props) => {
     const { expand, ...other } = props;
     return <IconButton {...other} />;
-  })(({ theme, expand }) => ({
+})(({ theme, expand }) => ({
     marginLeft: 'auto',
     transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.shortest,
+        duration: theme.transitions.duration.shortest,
     }),
-  }));
+}));
 
 
-function Post(props){
-    const{userId, userName, title, text} = props;
+function Post(props) {
+    const { userId, userName, title, text } = props;
 
     const [expanded, setExpanded] = React.useState(false);
     const [liked, setLiked] = useState(false);
 
     const handleExpandClick = () => {
-      setExpanded(!expanded);
+        setExpanded(!expanded);
     };
 
     const handleLike = () => {
         setLiked(!liked);
     }
-    return(
+    return (
         <div className="postContainer">
-            <Card style={{ width: 800, textAlign:'left', margin:20}}>
-      <CardHeader
-        avatar={
-          
-            <Link style={{textDecoration: 'none'}} to={{ pathname : '/users/' +userId}}>
-                <Avatar style={{ background: 'linear-gradient(45deg, #2196F3  30%, #21CBF3 90%)',
-        color: 'white' }} aria-label="recipe">
-                    {userName.charAt(0).toUpperCase()}
-                </Avatar>
-            </Link>
-        }
-        title={title}
-      />
-      <CardContent>
-        <Typography variant="body2" color="text.secondary">
-          {text}
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <IconButton 
-        onClick={handleLike}
-        aria-label="add to favorites">
-          <FavoriteIcon style={liked?{color:"red"}:null}/>
-        </IconButton>
-        <ExpandMore
-          expand={expanded}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <CommentIcon />
-        </ExpandMore>
-      </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-         
-        </CardContent>
-      </Collapse>
-    </Card>
+            <Card style={{ width: 800, textAlign: 'left', margin: 20 }}>
+                <CardHeader
+                    avatar={
+
+                        <Link style={{ textDecoration: 'none' }} to={{ pathname: '/users/' + userId }}>
+                            <Avatar style={{
+                                background: 'linear-gradient(45deg, #2196F3  30%, #21CBF3 90%)',
+                                color: 'white'
+                            }} aria-label="recipe">
+                                {userName.charAt(0).toUpperCase()}
+                            </Avatar>
+                        </Link>
+                    }
+                    title={title}
+                />
+                <CardContent>
+                    <Typography variant="body2" color="text.secondary">
+                        {text}
+                    </Typography>
+                </CardContent>
+                <CardActions disableSpacing>
+                    <IconButton
+                        onClick={handleLike}
+                        aria-label="add to favorites">
+                        <FavoriteIcon style={liked ? { color: "red" } : null} />
+                    </IconButton>
+                    <ExpandMore
+                        expand={expanded}
+                        onClick={handleExpandClick}
+                        aria-expanded={expanded}
+                        aria-label="show more"
+                    >
+                        <CommentIcon />
+                    </ExpandMore>
+                </CardActions>
+                <Collapse in={expanded} timeout="auto" unmountOnExit>
+                    <CardContent>
+
+                    </CardContent>
+                </Collapse>
+            </Card>
         </div>
     )
-    
+
 }
 
 export default Post;
