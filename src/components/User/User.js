6791d41a4f0a64469f3a7c012217334d24138b28
Hasan Huseyin Semiz <hhsem49@gmail.com>
2022-12-13 01:50:44 +0100

    update

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,9 +1,9 @@
 import React from "react";
-import {useParams} from "react-router-dom"
+import { useParams } from "react-router-dom"
 
 function User() {
-    const {userId} = useParams();
-    return(
+    const { userId } = useParams();
+    return (
         <div>
             User! {userId}
         </div>
