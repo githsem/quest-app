af468fb689dbbf4a741d02a635589d30273cc3f3
Hasan Huseyin Semiz <hhsem49@gmail.com>
2023-01-07 23:02:34 +0100

    edited

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -4,9 +4,7 @@ import { useParams } from "react-router-dom"
 function User() {
     const { userId } = useParams();
     return (
-        <div>
-            User! {userId}
-        </div>
+        
     )
 }
 
